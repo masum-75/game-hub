f8ee5fe19e8be0ca9f241ed6ba666c95644e4845
masum-75 <tanbirmasum014@gmail.com>
2025-10-25 08:36:28 +0600

    implement my profile section

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import MyProfile from "../pages/MyProfile";
 
 const HomeLayout = () => {
   return (
@@ -9,10 +10,12 @@ const HomeLayout = () => {
       <header className="border-b border-gray-800">
         <div className="w-11/12 mx-auto py-4">
           <Navbar />
+          
         </div>
       </header>
 
       <main className="w-11/12 mx-auto py-8">
+      <MyProfile></MyProfile>
         <Outlet />
       </main>
 
--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
+import { useNavigate } from "react-router";
+import userIcon from "../assets/user.png"
+
 
 const MyProfile = () => {
-    return (
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  return (
+    <div className="bg-[#0b0b0b] p-6 rounded">
+      <div className="flex items-center gap-6">
+        <img src={user?.photoURL || userIcon} className="w-28 h-28 rounded-full object-cover" />
         <div>
-            
+          <h2 className="text-2xl font-bold">{user?.displayName || "No Name"}</h2>
+          <p className="text-gray-400">{user?.email}</p>
+          <button className="btn btn-outline btn-sm mt-3" onClick={() => navigate("/profile/update")}>Update Info</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MyProfile;
